25f2006e73e87916b090f2179ef18c8dba6a3570
Mitsuki Oishi <thickyheadge@gmail.com>
2023-03-19 15:11:12 +0900

    寿司ピザの追加

--- index.js
+++ index.js
@@ -23,9 +23,15 @@ const server = http
             firstItem: 'ごはん',
             secondItem: 'パン'
           }));
+        } else if (req.url === '/enquetes/sushi-pizza') {
+          res.write(pug.renderFile('./form.pug', {
+            path: req.url,
+            firstItem: '寿司',
+            secondItem: 'ピザ'
+          }));
         }
-        res.end();
-        break;
+          res.end();
+          break;
       case 'POST':
         let rawData = '';
         req
